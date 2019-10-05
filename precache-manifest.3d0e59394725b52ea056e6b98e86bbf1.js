825e55973ed8f087162cac4d33cd2b65292b365f
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-10-05 11:33:18 -0300

    Deploy

---
+++ precache-manifest.3d0e59394725b52ea056e6b98e86bbf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a520da8c986471926afe096e7935f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4e40d803941d8c7b9e",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "45fb954fcc7c7000a1b5",
+    "url": "/static/css/main.879281ec.chunk.css"
+  },
+  {
+    "revision": "6f4e40d803941d8c7b9e",
+    "url": "/static/js/2.5feee028.chunk.js"
+  },
+  {
+    "revision": "45fb954fcc7c7000a1b5",
+    "url": "/static/js/main.742c2512.chunk.js"
+  },
+  {
+    "revision": "ae79717488a8e0542761",
+    "url": "/static/js/runtime~main.b78a8ba4.js"
+  }
+]);
